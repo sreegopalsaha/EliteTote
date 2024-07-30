f3b859bedd41feca86cdc78078a9cc0066b73462
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-07-30 15:17:57 +0530

    Set up Express.js middleware and view engine

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const db = require('./configs/mongooseConnection');
+const path = require('path');
+const staticPath = path.join(__dirname, 'public');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(staticPath));
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res)=>{
     res.send('Hello from the server!');
