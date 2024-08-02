d33ab81e4ca240174c97677bad90e41bf3cacb0b
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-02 17:30:49 +0530

    add route for creating products

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const productModel = require('../models/product.model');
+const {isAdminLoggedIn} = require('../middlewares/isLoggedIn');
+const upload = require('../configs/multerConfig');
 
 router.get('/', (req, res)=>{
     res.send('This is product router');
 });
 
+router.post('/create', isAdminLoggedIn, upload.single('productImg'), async (req, res)=>{
+    const { name, image, price, textcolor, bgcolor, panelcolor, discount} = req.body;
+    let product = await productModel.create({
+        name,
+        image: req.file.filename,
+        price,
+        textcolor,
+        bgcolor,
+        panelcolor,
+        discount
+    });
+    res.redirect('/admin');
+});
 
 module.exports = router;
