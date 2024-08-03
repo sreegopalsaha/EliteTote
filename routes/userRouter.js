3426ba6b3aece3ca3270b9169bd374896dc10dd6
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-03 16:19:14 +0530

    add cart routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {loginUser, registerUser, logoutUser} = require('../controllers/userController');
 const upload = require('../configs/multerConfig');
+const { isUserLoggedIn } = require('../middlewares/isLoggedIn');
+const userModel = require('../models/user.model');
 
 router.get('/', (req, res)=>{
     res.send('This is user router');
@@ -10,5 +12,17 @@ router.get('/', (req, res)=>{
 router.post('/login', loginUser);
 router.post('/register', upload.single('image'), registerUser);
 router.get('/logout', logoutUser);
+
+router.get('/addcart/:productId', isUserLoggedIn, async (req, res)=>{
+    let user = await userModel.findById(req.user.id);
+    user.cart.push(req.params.productId);
+    await user.save();
+    res.redirect('/home');
+});
+
+router.get('/cart', isUserLoggedIn, async(req, res)=>{
+    const user = await userModel.findById(req.user.id).populate('cart');
+    res.render('cart', {product: user.cart});
+});
 
 module.exports = router;
